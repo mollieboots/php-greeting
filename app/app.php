ce0c8a3e12086eaad9c25c0c606d0f28e48281d3
Jason Awbrey and Mollie Bootsma <student@epicodus.com>
2016-02-09 08:36:10 -0800

    did the thing

--- app/app.php
+++ app/app.php
@@ -3,5 +3,16 @@
 
     $app = new Silex\Application();
 
+    $app->get("/", function() {
+      $message = strtoupper("oh hey");
+      return "<html><head></head><body><a href='/goodbye'>Goodbye</a><br>$message";
+
+    });
+
+    $app->get("/goodbye", function(){
+      $message = "goodbye";
+      return "<html><head></head><body><a href='/'>Oh Hey</a><br>$message";
+    });
+
     return $app;
 ?>
